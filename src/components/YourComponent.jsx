ce11a84a78db1fb430f57a2aff74a9a52c11bb48
Alexander Meshok <AlexanderMeshok@coherentsolutions.com>
2019-03-29 18:34:39 +0300

    add generate function

--- src/components/YourComponent.jsx
+++ src/components/YourComponent.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import { findMaxAdjacent, getRandomValue } from './utilities';
+import { findMaxAdjacent, getRandomValue, generateTableData } from './utilities';
 import './YourComponent.less';
 
+function data() {
+    // return [[4,33,321,4,33,321,33,321],
+    //     [4,33,321,4,33,321,33,321],
+    //     [4,33,321,4,33,321,33,321],
+    //     [4,33,321,4,33,321,33,321],
+    //     [4,33,321,4,33,321,33,321],
+    //     [4,33,321,4,33,321,33,321],
+    //     [4,33,321,4,33,321,33,321],
+    //     [4,33,321,4,33,321,33,321]];
+    console.log(generateTableData(3));
+    return generateTableData(3);
+}
+
 /* Here goes your component */
-export default () => <h1>Hello World</h1>;
+export default () => <h1>Hello World { data() }</h1>;
--- src/components/utilities.js
+++ src/components/utilities.js
@@ -1,13 +1,16 @@
 export function findMaxAdjacent(data, lim) {
   const size = data[0].length;
+  
   let result = {res: 1, indexes: []};
   let vectors = new Array(8).fill(0);
   let directionsMap = [];
   let tempProd;
+  
 
   const isIndexCorrect = (x, y) => x < size && y < size && x >= 0 && y >= 0;
 
   for (let i = 0; i < size; i += 1) {
+    
     for (let j = 0; j < size; j += 1) {
       vectors = vectors.map(() => ({res: 1, indexes: []}));
       for (let k = 0; k < lim; k += 1) {
@@ -25,6 +28,16 @@ export function findMaxAdjacent(data, lim) {
     }
   }
   return result;
+}
+
+export function generateTableData(size) {
+  
+  const repeat = (fn, n) => Array(n).fill(0).map(fn);
+  const rand = () => getRandomValue(1,100);
+  const data = n => repeat(() => repeat(rand, n), n);
+
+  console.log(data(size));
+
 }
 
 export const getRandomValue = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
