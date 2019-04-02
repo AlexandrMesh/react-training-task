70dfbaf2d43ba4daa48334a6748c0d8c43f4304d
Alexander Meshok <AlexanderMeshok@coherentsolutions.com>
2019-04-02 13:26:12 +0300

    added error class

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Input.less';
 
+const errorClass = (error, from) => {
+    if (error) {
+        return 'error';
+    }
+}
+
 const Input = (props) => {
     return (
         <div className="input-wrapper">
             <label htmlFor={props.id}>{props.label}</label>
-            <input onChange={(event) => props.changeValue(event)} id={props.id} type="number" value={props.value} step="1" />
-            { props.error && <div className="error">{props.error}</div>}
+            <input onChange={(event) => props.changeValue(event)} className={errorClass(props.error)} id={props.id} type="number" value={props.value} step="1" />
+            { props.error && <div className={errorClass(props.error)}>{props.error}</div>}
             
         </div>
     )
